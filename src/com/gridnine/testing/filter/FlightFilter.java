2e521e59d20d01a94c629f7c0f2c33287ab11439
IgorPushka <igor081991@yandex.ru>
2024-10-15 13:13:27 +0300

    FlightFilter.java fix imports

--- src/com/gridnine/testing/filter/FlightFilter.java
+++ src/com/gridnine/testing/filter/FlightFilter.java
@@ -3,10 +3,8 @@ package com.gridnine.testing.filter;
 import com.gridnine.testing.model.Flight;
 import com.gridnine.testing.model.Segment;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 public interface FlightFilter {
     List<Flight> flightFilter(List<Flight> flights, Predicate<Flight>... flightPredicates);
